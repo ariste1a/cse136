53ac4a7f43a39c7ac6e8af91a32b0a39ded2e8b8
Jonathan Lin <jonathanlin.m@gmail.com>
2013-08-21 23:44:12 -0700

    testing staff tests
    testception (might need to roll this back)

---
+++ CSE136/SL136/SL136/POCO/Staff.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization; // this is required
+
+namespace POCO
+{
+    [DataContract]
+    public class Staff
+    {
+        [DataMember]
+        public string id { get; set; }
+
+        [DataMember]
+        public string email { get; set; }
+
+        [DataMember]
+        public string password { get; set; }
+
+    }
+}

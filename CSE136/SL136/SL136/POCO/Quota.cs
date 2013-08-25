181a53b6067822afa4b7870ac2c8e5d5892cca09
Jonathan Lin <jonathanlin.m@gmail.com>
2013-08-25 02:18:41 -0700

    quota pocco
    hueheuhe

---
+++ CSE136/SL136/SL136/POCO/Quota.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization; // this is required
+
+namespace POCO
+{
+    [DataContract]
+    public class Quota
+    {
+        [DataMember]
+        public string schedule_id { get; set; }
+
+        [DataMember]
+        public string course_title { get; set; }
+
+        [DataMember]
+        public string students_enrolled { get; set; }
+
+        [DataMember]
+        public string max_students { get; set; }
+
+    }
+}

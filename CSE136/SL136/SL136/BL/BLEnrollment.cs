1e44022d7dd2709c8b3f084a7186cbbd56d71522
Jonathan Lin <jonathanlin.m@gmail.com>
2013-08-29 23:55:02 -0700

    bl staff + enrallmant

---
+++ CSE136/SL136/SL136/BL/BLEnrollment.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using POCO;
+using DAL;
+
+
+namespace BL
+{
+    public class BLEnrollment
+    {
+        public static string InsertEnrollment(Enrollment e, ref List<string> errors)
+        {
+            string id = DALEnrollment.InsertEnrollment(e, ref errors);
+            if (id != null)
+            {
+                return id;
+            }
+            return "-1";
+        }
+
+        public static Enrollment GetEnrollment(string student_id, string schedule_id, ref List<string> errors)
+        {
+            return DALEnrollment.GetEnrollmentGrade(student_id, schedule_id, ref errors);
+        }
+
+       
+        public static void UpdateEnrollment(Enrollment en, ref List<string> errors)
+        {
+            DALEnrollment.UpdateEnrollment(en, ref errors);
+        }
+         
+
+        public static void DeleteEnrollmentSchedule(string student_id, string schedule_id, ref List<string> errors)
+        {
+            DALEnrollment.DeleteEnrollmentSchedule(student_id, schedule_id, ref errors); 
+        }
+
+
+    }
+}
---
+++ CSE136/SL136/SL136/BL/BLStaff.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using POCO;
+using DAL;
+
+
+namespace BL
+{
+    class BLStaff
+    {
+        public static string InsertStaff(Staff s, ref List<string> err)
+        {
+            string ret_id = DALStaff.InsertStaff(s, ref err);
+            return ret_id != null ? ret_id : "-1"; 
+        }
+
+        public static void UpdateStaff(Staff s, ref List<string> err)
+        {
+            DALStaff.UpdateStaff(s, ref err);
+        }
+
+        public static void DeleteStaff(string id, ref List<string> err)
+        {
+            DALStaff.DeleteStaff(id, ref err);
+        }
+
+        public static void GetStaffDetails(string email, ref List<string> err)
+        {
+            DALStaff.GetStaffDetail(email, ref err);
+        }
+    }
+}

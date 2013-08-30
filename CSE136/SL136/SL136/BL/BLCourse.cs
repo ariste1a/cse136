ac5aa49a4410851a2b5232ce72ce7f0045826250
Vincent Nguyen <atermagus312@gmail.com>
2013-08-29 23:39:44 -0700

    BLDiscussions + BLCourse

--- CSE136/SL136/SL136/BL/BLCourse.cs
+++ CSE136/SL136/SL136/BL/BLCourse.cs
@@ -14,7 +14,7 @@ namespace BL
     {
       return (DALCourse.GetCourseList());
     }
-    public static string InsertCourse(Course course, List<string> errors)
+    public static string InsertCourse(Course course, ref List<string> errors)
     {
         string id = DALCourse.InsertCourse(course, ref errors);
         if (id != null)
@@ -23,16 +23,16 @@ namespace BL
         }
         return "-1";
     }
-    public static Course GetCourse(string id, List<string> errors)
+    public static Course GetCourse(string id, ref List<string> errors)
     {
         Course course = DALCourse.GetCourse(id, ref errors);
         return course; 
     }
-    public static void DeleteCourse(string id, List<string> errors)
+    public static void DeleteCourse(string id, ref List<string> errors)
     {
         DALCourse.DeleteCourse(id, ref errors); 
     }
-    public static void UpdateCourse(Course course, List<string> errors)
+    public static void UpdateCourse(Course course, ref List<string> errors)
     {
         DALCourse.UpdateCourse(course, ref errors);
     }    
---
+++ CSE136/SL136/SL136/BL/BLDiscussion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using POCO;
+using DAL;
+
+namespace BL
+{
+    public class Discussion
+    {
+        public static List<Schedule> GetDiscussions(int class_id, ref List<string> errors)
+        {
+            return DALDiscussion.GetDiscussions(class_id, ref errors); 
+        }
+        public static string createDiscussion(int lecture_id, string session, int day, int time, int instructor, int quota, ref List<String> errors)
+        {
+            string id = createDiscussion(lecture_id, session, day, time, instructor, quota, ref errors);
+            return id; 
+        }
+        public static Boolean removeDiscussion(string discussion_id, ref List<String> errors)
+        {
+            return removeDiscussion( discussion_id, ref errors);
+        }
+    }
+}

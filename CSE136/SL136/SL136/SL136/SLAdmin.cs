a6708dde4ba10fc99cd2b6cd459128f9097b650b
Jonathan Lin <jonathanlin.m@gmail.com>
2013-08-31 14:01:34 -0700

    moar SL

---
+++ CSE136/SL136/SL136/SL136/SLAdmin.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using BL;
+using POCO;
+
+namespace SL136
+{
+    class SLAdmin
+    {
+        public static string InsertAdmin(Admin a, ref List<string> err)
+        {
+            return BLAdmin.InserAdmin(a, ref err);
+        }
+
+        public static void UpdateAdmin(Admin a, ref List<string> err)
+        {
+            BLAdmin.UpdateAdmin(a, ref err);
+        }
+
+        public static void DeleteAdmin(string id, ref List<string> err)
+        {
+            BLAdmin.DeleteAdmin(id, ref err);
+        }
+
+        public static void GetAdmin(string email, ref List<string> err)
+        {
+            BLAdmin.GetADmin(email, err);
+        }
+    }
+}
---
+++ CSE136/SL136/SL136/SL136/SLEnrollment.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using POCO;
+using BL;
+
+namespace SL136
+{
+    public class SLEnrollment
+    {
+        public static string InsertEnrollment(Enrollment e, ref List<string> err)
+        {
+            return BLEnrollment.InsertEnrollment(e, ref err);
+        }
+
+        public static Enrollment GetEnrollment(string student_id, string schedule_id, ref List<string> err)
+        {
+            return BLEnrollment.GetEnrollment(student_id, schedule_id, ref err);
+        }
+
+        public static void UpdateEnrollment(Enrollment e, ref List<string> err)
+        {
+            BLEnrollment.UpdateEnrollment(e, ref err);
+        }
+
+        public static void DeleteEnrollment(string student_id, string schedule_id, ref List<string> err)
+        {
+            BLEnrollment.DeleteEnrollmentSchedule(student_id, schedule_id, ref err);
+        }
+    }
+}

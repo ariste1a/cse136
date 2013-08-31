7978a013734eb9ffde5b860b27a62acf594d3bee
Jonathan Lin <jonathanlin.m@gmail.com>
2013-08-31 15:11:18 -0700

    sum testicles

---
+++ CSE136/SL136/SL136/BLTest/BLAdminTest.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using POCO;
+using BL;
+
+namespace BLTest
+{
+    [TestClass]
+    class BLAdminTest
+    {
+        public BLAdminTest()
+        {
+            // constructa rogic
+        }
+
+        private TestContext testContextInstance;
+
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        [TestMethod]
+        public void AdminErrorTest()
+        {
+            List<string> er = new List<string>();
+
+            BLAdmin.GetAdmin(null, ref er);
+            Assert.AreEqual(1, er.Count);
+        }
+
+        [TestMethod]
+        public void AdminCRUDTest()
+        {
+            Admin a = new Admin();
+            a.id = Guid.NewGuid().ToString().Substring(0, 20);
+            a.email = "pooplord@spiderman.com";
+            a.password = "password";
+
+            List<string> errors = new List<string>(); 
+
+            BLAdmin.InsertAdmin(a, ref errors);
+
+            Assert.AreEqual(0, errors.Count);
+
+            Admin fetchedAdmin = BLAdmin.GetAdmin(a.email, ref errors);
+
+            Assert.AreEqual(0, errors.Count);
+
+            Assert.AreEqual(a.id, fetchedAdmin.id); 
+            Assert.AreEqual(a.email, fetchedAdmin.email);
+            Assert.AreEqual(a.password, fetchedAdmin.password);
+
+            a.password = "banana";
+
+            BLAdmin.UpdateAdmin(a, ref errors);
+
+            Assert.AreEqual(0, errors.Count); 
+
+            fetchedAdmin = BLAdmin.GetAdmin(a.email, ref errors);
+
+            Assert.AreEqual(a.id, fetchedAdmin.id); 
+            Assert.AreEqual(a.email, fetchedAdmin.email);
+            Assert.AreEqual(a.password, fetchedAdmin.password);
+
+            BLAdmin.DeleteAdmin(a.id, ref errors); 
+            Assert.AreEqual(0, errors.Count); 
+
+            Admin nullAdmin = BLAdmin.GetAdmin(a.email, ref errors);
+            Assert.AreEqual(0, errors.Count); 
+
+            Assert.AreEqual(null, nullAdmin); 
+    }
+}
---
+++ CSE136/SL136/SL136/BLTest/BLCourseTest.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using POCO;
+using BL;
+
+namespace BLTest
+{
+    class BLCourseTest
+    {
+        public BLCourseTest()
+        {
+            // y d we even got these l0l
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        [TestMethod]
+        public void InsertCourseErrorTest()
+        {
+            List<string> errors = new List<string>();
+
+            BLCourse.InsertCourse(null, ref errors);
+            Assert.AreEqual(1, errors.Count);
+
+            errors = new List<string>();
+
+            Student student = new Student();
+            student.id = "";
+            BLStudent.InsertStudent(student, ref errors);
+            Assert.AreEqual(1, errors.Count);
+        }
+
+        [TestMethod]
+        public void CourseCRUDTest()
+        {
+            Course c = new Course();
+            c.id = Guid.NewGuid().ToString().Substring(0, 20);
+            c.title = "Introduction to Facepalm";
+            c.level = CourseLevel.lower;
+            c.description = "50 derps to every herp!";
+
+            List<string> errors = new List<string>();
+
+            BLCourse.InsertCourse(c, ref errors);
+
+            Assert.AreEqual(0, errors.Count);
+
+            Course fetchedCourse = BLCourse.GetCourse(c.id, ref errors);
+
+            Assert.AreEqual(0, errors.Count);
+
+            Assert.AreEqual(c.id, fetchedCourse.id);
+            Assert.AreEqual(c.title, fetchedCourse.title);
+            Assert.AreEqual(c.level, fetchedCourse.level);
+            Assert.AreEqual(c.description, fetchedCourse.description);
+
+            c.title = "banana";
+
+            BLCourse.UpdateCourse(c, ref errors);
+
+            Assert.AreEqual(0, errors.Count);
+
+            fetchedCourse = BLCourse.GetCourse(c.id, ref errors);
+
+            Assert.AreEqual(c.id, fetchedCourse.id);
+            Assert.AreEqual(c.title, fetchedCourse.title);
+            Assert.AreEqual(c.level, fetchedCourse.level);
+            Assert.AreEqual(c.description, fetchedCourse.description);
+
+            BLCourse.DeleteCourse(c.id, ref errors);
+            Assert.AreEqual(0, errors.Count);
+
+            Course nullCourse = BLCourse.GetCourse(c.id, ref errors);
+            Assert.AreEqual(0, errors.Count);
+
+            Assert.AreEqual(null, nullCourse);
+        }
+
+    }
+}
